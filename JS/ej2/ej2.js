f394974236d1ea7bd71a0f5117cf8e554124797f
David Garcia Soto <David.gcia.soto@gmail.com>
2024-04-12 14:31:50 +0200

    ejercicio 2 js terminado

--- JS/ej2/ej2.js
+++ JS/ej2/ej2.js
@@ -1,3 +1,14 @@
 let contenido = document.getElementById("contenido")
-contenido.style.backgroundColor = "red";
-contenido.style.color = "red";
+const arrayFondo = ["red","blue","green","yellow"]
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+setInterval(
+    function cambioColor(){
+        let numero = getRandom(0,3)
+        let fondo = arrayFondo[numero]
+        contenido.style.backgroundColor = fondo
+        contenido.style.color = fondo
+},1000)
+
