0195b4f287296623bdf645c2dcf87dc1eecf0902
David Garcia Soto <David.gcia.soto@gmail.com>
2024-04-12 15:07:16 +0200

    ejercicio 3 js terminado

---
+++ JS/ej3/ej3.js
@@ -0,0 +1,8 @@
+let contenido = document.getElementById("contenido")
+let horaActual = new Date();
+console.log(horaActual.ge)
+setInterval(
+    function segundos(){
+        let horaActual = new Date();
+        contenido.innerText = horaActual.getSeconds()
+},1000)
